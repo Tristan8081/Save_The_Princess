75064baa7d6e2c79050af17b42320fa428f08815
Tristan Scharfenstein-Montgomery <tristanrockshawaii@gmail.com>
2016-11-09 14:41:10 -1000

    Update Walls
    Updated walls and hiding spots in the maze for all levels

--- Save_The_Princess.ino
+++ Save_The_Princess.ino
@@ -8,9 +8,7 @@ void setup()
 }
 
 void loop() 
-{
-  DrawPx (0,0,Red);    //Draw Player/Mario
-  DisplaySlate();     //Draw on screen
+{ 
   
    //check to see if a button was pressed
    CheckButtonsDown();
@@ -45,116 +43,134 @@ void loop()
         x = 7;
 }
 
+
+
+
 void Drawlevel1()  //maze walls for the first level
 {
-  DrawPx(0,2,Violet); 
-  DrawPx(0,3,Violet);
-  DrawPx(0,4,Violet);
 
-  DrawPx(1,0,Violet);
-  DrawPx(1,2,Violet);
-  DrawPx(1,6,Violet);
+  DrawPx (0,0,Red);    //Draw Player/Mario
+  
+  DrawPx(0,2,6); //maze walls for the first level
+  DrawPx(0,3,6);
+  DrawPx(0,4,6);
 
-  DrawPx(2,2,Violet);
-  DrawPx(2,4,Violet);
-  DrawPx(2,5,Violet); //maze walls for the first level
+  DrawPx(1,0,6);
+  DrawPx(1,2,6);
+  DrawPx(1,6,6);
 
-  DrawPx(3,1,Violet);
-  DrawPx(3,2,Violet);
-  DrawPx(3,4,Violet);
-  DrawPx(3,5,Violet); //maze walls for the first level
-  DrawPx(3,7,Violet);
+  DrawPx(2,2,6);
+  DrawPx(2,4,5);     //Hiding spot
+  DrawPx(2,5,6); 
+
+  DrawPx(3,1,6);
+  DrawPx(3,2,6);
+  DrawPx(3,4,6);
+  DrawPx(3,5,6);
+  DrawPx(3,7,6);
  
-  DrawPx(4,4,Violet);
-  DrawPx(4,5,Violet);
-  DrawPx(4,7,Violet); //maze walls for the first level
+  DrawPx(4,4,6);
+  DrawPx(4,5,5);     //Hiding spot
+  DrawPx(4,7,6); 
 
-  DrawPx(5,0,Violet);
-  DrawPx(5,1,Violet); //maze walls for the first level
-  DrawPx(5,3,Violet);
-  DrawPx(5,7,Violet);
+  DrawPx(5,0,6);
+  DrawPx(5,1,6); 
+  DrawPx(5,3,6);
+  DrawPx(5,7,6);
 
-  DrawPx(6,3,Violet);
-  DrawPx(6,5,Violet);
-  DrawPx(6,6,Violet);
-  DrawPx(6,7,Violet); //maze walls for the first level
-
-  DrawPx(7,0,Violet);
-  DrawPx(7,1,Violet);
-  DrawPx(7,3,Violet);
-  DrawPx(7,7,White); //dot that you eat to go to next level
+  DrawPx(6,3,6);
+  DrawPx(6,5,6);
+  DrawPx(6,6,6);
+  DrawPx(6,7,6); 
+  
+  DrawPx(7,0,6);
+  DrawPx(7,1,6);
+  DrawPx(7,3,6);
+  DrawPx(7,7,7); //dot that you eat to go to next level
 
   DisplaySlate();
 }
 
 void Drawlevel2() //draws the second level to the game slate
 {
-DrawPx(1,7,16); //maze walls of level 2
-DrawPx(1,0,16);
-DrawPx(1,1,16);
 
-DrawPx(2,6,16);
-DrawPx(2,16,16);
-DrawPx(2,2,16);
-DrawPx(2,3,16);
+  DrawPx(4,0,Red);    //Mario/Player
+  
+  DrawPx(1,7,6); //maze walls of level 2
+  DrawPx(1,0,6);
+  DrawPx(1,1,6);
 
-DrawPx(3,4,16); //maze walls of level 2
-DrawPx(3,5,16);
+  DrawPx(2,6,6);
+  DrawPx(2,5,6);
+  DrawPx(2,2,6);
+  DrawPx(2,3,6);
+  DrawPx(2,4,6);
+  DrawPx(2,1,6);
 
-DrawPx(4,7,16);
-DrawPx(4,0,16);
-DrawPx(4,1,16);
-DrawPx(4,2,16);
+  DrawPx(3,4,5);  //Hiding Spot
+  DrawPx(3,5,6);
+  DrawPx(3,2,6);
 
-DrawPx(5,2,16);
-DrawPx(5,3,16); //maze walls of level 2
-DrawPx(5,5,16);
-DrawPx(5,6,16);
+  DrawPx(4,7,6);
+  DrawPx(4,0,6);
+  DrawPx(4,1,6);
+  DrawPx(4,2,6);
 
-DrawPx(6,4,16);
-DrawPx(6,5,16);
+  DrawPx(5,1,6);
+  DrawPx(5,2,6);
+  DrawPx(5,3,6);
+  DrawPx(5,5,6);
+  DrawPx(5,6,6);
 
-DrawPx(2,7,White);//dot that you eat to the next level
+  DrawPx(6,1,6)
+  DrawPx(6,4,6);
+  DrawPx(6,5,6);
+  DrawPx(6,3,6);
+  DrawPx(6,2,5);    //Hiding Spot
 
-DisplaySlate();
+  DrawPx(7,7,7);  //dot that you eat to the next level
+
+  DisplaySlate();
 }
 
 void Drawlevel3() //draws the second level to the game slate
 {
-DrawPx(0,0,Violet); //maze walls of level 3
-DrawPx(0,5,Violet);
-DrawPx(1,3,Violet);
-DrawPx(1,6,Violet);
 
-DrawPx(2,0,Violet);
-DrawPx(2,2,Violet);
-DrawPx(2,2,Violet);
-DrawPx(2,4,Violet); //maze walls of level 3
-DrawPx(2,5,Violet);
+  DrawPx(0,2,Red);    //Mario/Player
 
-DrawPx(3,1,Violet);
-DrawPx(3,2,Violet);
-DrawPx(3,5,Violet);
-DrawPx(3,6,Violet);
-DrawPx(3,7,Violet);
+  DrawPx(0,0,6); //maze walls of level 3
+  DrawPx(0,5,6);
+  DrawPx(1,3,6);
+  DrawPx(1,6,6);
 
-DrawPx(4,3,Violet); //maze walls of level 3
-DrawPx(4,7,Violet);
+  DrawPx(2,0,6);
+  DrawPx(2,2,6);
+  DrawPx(2,3,6);
+  DrawPx(2,4,6); 
+  DrawPx(2,5,6);
 
-DrawPx(5,0,Violet);
-DrawPx(5,2,Violet);
-DrawPx(5,1,Violet);
-DrawPx(5,4,Violet);
-DrawPx(5,6,Violet);
+  DrawPx(3,1,6);
+  DrawPx(3,2,6);
+  DrawPx(3,5,6);
+  DrawPx(3,6,6);
+  DrawPx(3,7,6);
 
-DrawPx(6,3,Violet);
-DrawPx(6,5,Violet);
+  DrawPx(4,3,6);
+  DrawPx(4,4,6); 
+  DrawPx(4,7,6);
 
-DrawPx(7,1,Violet); //maze walls of level 3
-DrawPx(7,7,Violet);
+  DrawPx(5,0,6);
+  DrawPx(5,2,6);
+  DrawPx(5,1,6);
+  DrawPx(5,4,6);
+  DrawPx(5,6,6);
+  
+  DrawPx(6,5,6);
+  DrawPx(7,1,6); 
+  DrawPx(7,7,6);
 
-DrawPx(5,7,DimRed);
+  DrawPx(5,7,8);
 
-DisplaySlate();
+  DisplaySlate();
 }
 
